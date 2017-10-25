a398b58a702c3ffca8af823fcec782a651c4a9db
andrujoh <andru_joh@yahoo.no>
2017-10-25 23:01:45 +0200

    Add theme, comics controller, images and layout page

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,12 +6,23 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller
+  public class ComicBooksController : Controller
+  {
+    // GET: ComicBooks
+    public ActionResult Detail()
     {
-        // GET: ComicBooks
-        public ActionResult Detail()
-        {
-            return View();
-        }
+      ViewBag.SeriesTitle = "The Amazing Spider-Man";
+      ViewBag.IssuesNumber = "#700";
+      ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+      ViewBag.Artists = new string[]
+      {
+        "Script: Dan Slott",
+        "Pencils: Humberto Ramos",
+        "Inks: Victor Olazaba",
+        "Colors: Edgar Delgado",
+        "Letters: Chris Eliopoulos"
+      };
+      return View();
     }
+  }
 }
